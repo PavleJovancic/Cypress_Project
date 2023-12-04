bba1c863e66a2f68629161de301e7e175a74e975
PavleJovancic <drjovancicpavle@gmail.com>
2023-12-04 15:52:35 +0100

    Add verify navigation to login page test

--- cypress/e2e/loginTests.js
+++ cypress/e2e/loginTests.js
@@ -6,5 +6,13 @@ describe('Login page tests', () => {
         cy.openHomePage()
     })
 
-    
+    it.only('Verify navigation to login page', () => {
+
+        navigateTo.loginPage()
+        cy.get('.display-2').should('contain', 'Login')
+        cy.url().then(url => {
+            cy.wrap(url).should('contain','/login')
+        })
+        
+    })
 })
