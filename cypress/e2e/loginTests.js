d03e638957222e65b888ff11c2247d1929f0879c
PavleJovancic <drjovancicpavle@gmail.com>
2023-12-04 15:28:32 +0100

    Add beforeEach method to loginTests

--- cypress/e2e/loginTests.js
+++ cypress/e2e/loginTests.js
@@ -1,6 +1,10 @@
-
+import { navigateTo } from "../support/page_objects/navigationPage"
 
 describe('Login page tests', () => {
+
+    beforeEach('open application', () => {
+        cy.openHomePage()
+    })
 
     
 })
