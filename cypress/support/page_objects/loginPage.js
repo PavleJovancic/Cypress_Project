cb61d9c5cf3496f4e33310e45b6af3e0b915ebcf
PavleJovancic <drjovancicpavle@gmail.com>
2023-12-04 16:27:58 +0100

    Add login method to loginPage

--- cypress/support/page_objects/loginPage.js
+++ cypress/support/page_objects/loginPage.js
@@ -1,5 +1,11 @@
 export class LoginPage {
     
+    login(email, password){
+        cy.get('#email').type(email)
+        cy.get('#password').type(password)
+        cy.get('[type="submit"]').click()
+    }
+    
 }
 
 export const onLoginPage = new LoginPage() 
